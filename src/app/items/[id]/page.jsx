da0ad73f5d362fda23568ae33fb4300803f4c001
Shahriar262 <srh.2622@gmail.com>
2026-01-18 01:04:40 +0600

    fix responsive issues

--- src/app/items/[id]/page.jsx
+++ src/app/items/[id]/page.jsx
@@ -42,7 +42,7 @@ export default async function ItemPage({ params }) {
               src={item.image}
               alt={item.name}
               fill
-              className="object-cover"
+              className="md:object-cover object-fill"
               priority
             />
           </div>
